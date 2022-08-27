372c8a3cb0364f6ea8bc29b38b2b8fe9521fbabf
Saradhi <saradhi0001069@gmail.com>
2022-08-27 11:26:53 +0530

    done

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const connection = require("./db/db");
-const express = require("express");
+const express = require("express")
 const cors = require("cors");
-const PatientsRoute = require("./routes/PatientsRoute");
-const UserRoute = require("./routes/UserRoute");
-const MedicineRoute = require("./routes/MedicineRoute");
-// App creation
-const app = express();
-//Needed middlewares
+const connection = require('./db/db')
+const Redirectroute = require("./routes/RedirectRoute");
+const Urlroute = require("./routes/UrlRoute");
+
+
+//app creation
+let app = express();
+
+// needed middlewares
+app.use(
+  cors({
+    origin: ["http://localhost:3000","http://localhost:3001"],
+  })
+);
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(
-    cors({
-      origin: ["http://localhost:3000","https://subtle-bublanina-b68415.netlify.app"],
-    })
-  );
-app.use("/patient",PatientsRoute);
-app.use("/user",UserRoute);
-app.use("/medicine",MedicineRoute);
-//testing
-app.get("/",(req,res)=>{
-  res.send("Mock11 working....")
-})
+app.use('/', Redirectroute)
+app.use('/api/url', Urlroute)
+
 
 // starting the server && checking db connection
-const PORT = process.env.PORT || 8060
+const PORT = process.env.PORT || 5000
 app.listen(PORT, async () => {
     try {
       await connection;
@@ -31,6 +29,5 @@ app.listen(PORT, async () => {
     } catch (err) {
       console.log(err);
     }
-    console.log("Backend is working at http://localhost:8060");
+    console.log("Backend is working at http://localhost:5000");
 });
-
